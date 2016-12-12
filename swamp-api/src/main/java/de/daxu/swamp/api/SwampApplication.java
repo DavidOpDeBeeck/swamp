743cb22d2044cc794d188b654d834b243831a64b
David Op de Beeck <davidopdebeeck@hotmail.com>
2016-12-12 17:45:27 +0100

    Removed duplicate annotation and changed deprecated EntityScan package.

--- swamp-api/src/main/java/de/daxu/swamp/api/SwampApplication.java
+++ swamp-api/src/main/java/de/daxu/swamp/api/SwampApplication.java
@@ -2,16 +2,12 @@ package de.daxu.swamp.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @EntityScan( { "de.daxu.swamp", "org.axonframework.eventstore.jpa" } )
-@ComponentScan( basePackages = { "de.daxu.swamp" } )
 @EnableJpaRepositories( basePackages = { "de.daxu.swamp" } )
-@SpringBootApplication( scanBasePackages = "de.daxu.swamp.api" )
+@SpringBootApplication( scanBasePackages = "de.daxu.swamp" )
 public class SwampApplication {
 
     public static void main( String[] args ) {
